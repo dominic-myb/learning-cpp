4d197c66b79d8f3415c72332fa105d3ddb41ae0d
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-04-19 03:38:30 +0800

    [upd] random.cpp

--- reviewer/random.cpp
+++ reviewer/random.cpp
@@ -2,13 +2,33 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
+void chooseDifficulty() {
+	cout << "[1] - Easy\n";
+	cout << "[2] - Normal\n";
+	cout << "[3] - Hard\n";
+	cout << "Select Difficulty: ";
+}
 int main() {
 	srand(time(0));
+	int opt = 0;
+	int lives;
 	int guess = 0;
 	int numberOfGuesses = 0;
-	int lives = 7;
 	int target = (rand() % 101) + 1;
+	do {
+		chooseDifficulty();
+	}while (!(cin >> opt) || (opt <= 0 || opt >= 4));
+	switch(opt){
+		case 1:
+			lives = 10;
+			break;
+		case 2:
+			lives = 7;
+			break;
+		case 3:
+			lives = 5;
+			break;
+	}
 	do {
 		cout << "Lives: " << lives << endl;
 		cout << "Guess: ";
@@ -31,6 +51,7 @@ int main() {
 	}while(lives > 0);
 	if (lives <= 0){
 		cout << "You lose!\n";
+		cout << "The Number is " << target;
 	}
 	
 	return 0;
