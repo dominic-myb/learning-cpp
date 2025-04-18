02919c5a2819b9539094c5b9bb03368a1e86583b
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-04-18 18:44:52 +0800

    [upd] leap_year.cpp

--- reviewer/leap_year.cpp
+++ reviewer/leap_year.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
+bool isLeapYear(int n){
+	return ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0);
+}
 int main() {
-	int year = 0;
-	cout << "Enter a year: ";
+	int year;
+	cout << "Enter year: ";
 	while(!(cin >> year)){
 		cin.clear();
 		cin.ignore(1000, '\n');
 	}
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+	if(isLeapYear(year)){
 		cout << year << " is a leap year.";
-	} else {
+	} 
+	else {
 		cout << year << " is not a leap year.";
 	}
 	return 0;
-}
+} 
