2a54f8a38dbcd64fdd2baf131b7b15922753493f
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-05-11 03:51:02 +0800

    [upd] seconds_to_hours.cpp

--- time-convert/seconds_to_hours.cpp
+++ time-convert/seconds_to_hours.cpp
@@ -1,11 +1,24 @@
+/*
+ * Author: Dominic Esguerra
+ * Last update: May. 11, 2025
+ * Description: Convert Seconds to Hours
+ */
+
 #include <iostream>
+#include <limits>
 #include <string>
 
-std::string convert(int secs) {
+std::string convert(float secs) {
   return "in Hours: " + std::to_string(secs / 60 / 60);
 }
 
 int main() {
-  std::cout << convert(3600);
+  float secs;
+  std::cout << "Seconds: ";
+  while (!(std::cin >> secs)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+  std::cout << convert(secs);
   return 0;
 }
