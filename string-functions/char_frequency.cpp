d0de8aff28078874183a6007193135c22e4e76bb
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-05-12 02:06:29 +0800

    [upd] added comments

--- string-functions/char_frequency.cpp
+++ string-functions/char_frequency.cpp
@@ -1,3 +1,9 @@
+/*
+ * Author: Dominic Esguerra
+ * Last update: May. 12, 2025
+ * Description: Outputs character frequency in a string
+ */
+
 #include <cctype>
 #include <iostream>
 #include <string>
@@ -5,16 +11,15 @@
 int charFrequency(const std::string &s, char target) {
   int res = 0;
   for (char c : s) {
-    if (tolower(c) == tolower(target)) {
+    if (tolower(c) == tolower(target))
       res++;
-    }
   }
   return res;
 }
 
 int main() {
-  std::string word = "banAna";
-  char target = 'a';
+  std::string word = "hello world";
+  char target = 'o';
   std::cout << "Word: " << word;
   std::cout << "\nCharacter: " << target;
   std::cout << "\nCharacter frequency: " << charFrequency(word, target);
